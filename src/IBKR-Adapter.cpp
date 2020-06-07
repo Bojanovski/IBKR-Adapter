9801fb200c7c6781bd25d34821aef348fcf7462f
Bojan Lovrovic <bojanlovrovic@gmail.com>
2020-06-07 22:55:32 +0100

    Initial commit

---
+++ src/IBKR-Adapter.cpp
@@ -0,0 +1,12 @@
+﻿// IBKR-Adapter.cpp : Defines the entry point for the application.
+//
+
+#include "IBKR-Adapter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
